b20763939e6829c5482a247c89f22057f91ea28d
KamilMarszalek <kamilmar553@gmail.com>
2025-04-18 11:10:55 +0200

    Added introduction text

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -1,7 +1,19 @@
 module Main where
 
-import Act1
+import Act1 (Lines, printLines, startAct1)
+
+introductionText :: Lines
+introductionText =
+  [ "INTRODUCTION:",
+    "Welcome to \"The Hidden Realm\", an interactive fiction adventure",
+    "set in the icy depths of Antarctica. Unravel ancient secrets,",
+    "face modern dangers, and shape your fate through your choices.",
+    "Don't be afraid to use HINTS - they will help you navigate",
+    "challenges and uncover the truth. Stay sharp - the unknown awaits.",
+    ""
+  ]
 
 main :: IO ()
 main = do
+  printLines introductionText
   startAct1
